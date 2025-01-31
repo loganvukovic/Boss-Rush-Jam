c06f0a5d8216e910684e6689e1f161abaa9b4f3b
BobJoeman21 <ccwhitford18@gmail.com>
2025-01-31 13:38:15 -0500

    CubeWIP

---
+++ Assets/TextureLogic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureLogic : MonoBehaviour
+{
+
+    public Texture2D[] faces;
+    public Material FaceMT;
+
+
+    public void ChangeFace(int FaceId)
+    {
+        //FaceMT.SetTexture(faces[FaceId]);
+    }
+}
