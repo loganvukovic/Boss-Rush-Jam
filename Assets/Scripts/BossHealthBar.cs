37bb73cb557ff6f7860ea5b3ff1bab6e68b80622
Logan Vukovic <loganv242@gmail.com>
2025-02-02 00:32:47 -0500

    Flashing health bar

--- Assets/Scripts/BossHealthBar.cs
+++ Assets/Scripts/BossHealthBar.cs
@@ -10,11 +10,18 @@ public class BossHealthBar : MonoBehaviour
     public float maxHealth;
     public float shownHealth;
     public Image healthBar;
+    public bool flashState;
+    public float flickerTimer;
+    public float flickerTime;
+    public float hue;
+    public float s;
+    public float v;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        flickerTime = 0.2f;
+        Color.RGBToHSV(new Color(healthBar.color.r, healthBar.color.g, healthBar.color.b), out hue, out s, out v);
     }
 
     // Update is called once per frame
@@ -26,10 +33,30 @@ public class BossHealthBar : MonoBehaviour
         if (shownHealth > curHealth)
         {
             shownHealth -= 30f * Time.deltaTime;
+            flickerTimer += Time.deltaTime;
         }
-        else shownHealth = curHealth;
+        else
+        {
+            shownHealth = curHealth;
+            flickerTimer = 0;
+            healthBar.color = Color.HSVToRGB(hue, 1f, 1f);
+        }
 
         healthBar.fillAmount = shownHealth / maxHealth;
+
+        if (flickerTimer > flickerTime)
+        {
+            if (!flashState)
+            {
+                flashState = true;
+                healthBar.color = Color.HSVToRGB(hue, 0.7f, 1f);
+            }
+            else
+            {
+                flashState = false;
+                healthBar.color = Color.HSVToRGB(hue, 1f, 1f);
+            }
+        }
 
         if (activeBoss.GetComponentInParent<BossActions>().dying && activeBoss.isPuppet)
         {
